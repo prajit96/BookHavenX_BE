6a666d886015c662b0736a7ed5ca2bb55175ab2f
prajit96 <santraprajit@gmail.com>
2023-12-11 22:32:27 +0530

    others routes done, prepare to deploy

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const { connection } = require("./db");
 const { userRouter } = require("./routes/userRoute");
 const { bookRouter } = require("./routes/bookRoute");
 const { bookReviewRouter } = require("./routes/bookReviewRoute");
+const { readingListRouter } = require("./routes/readingListRoute");
+const { chatRouter } = require("./routes/chatbotRoute");
+const { cartRouter } = require("./routes/cartRoute");
+const { purchaseRouter } = require("./routes/purchaseRoute");
+const { communityDiscRouter } = require("./routes/communityDiscRoute");
+const { discussionPostRouter } = require("./routes/discussionPostRoute");
 
 const app = express();
 app.use(express.json());
@@ -11,6 +17,12 @@ app.use(cors());
 app.use("/users", userRouter);
 app.use("/books", bookRouter);
 app.use("/review", bookReviewRouter);
+app.use("/readinglists", readingListRouter);
+app.use("/chatbot", chatRouter);
+app.use("/cart", cartRouter);
+app.use("/purchase", purchaseRouter);
+app.use("/community", communityDiscRouter);
+app.use("/posts", discussionPostRouter);
 
 app.get("/", (req, res)=>{
     res.status(200).send("Welcome to BookHavenX Backend")
